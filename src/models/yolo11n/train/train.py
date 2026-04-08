190cd100f5823eb409c240a20803155e469f85b1
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-04-08 20:04:05 -0300

    :recycle: refactor: remoção do mlflow

--- src/models/yolo11n/train/train.py
+++ src/models/yolo11n/train/train.py
@@ -10,8 +10,6 @@ import torch
 import argparse
 from ultralytics import YOLO
 
-import mlflow
-
 def train_and_validate():
     parser = argparse.ArgumentParser(description="YOLO Training & Validation Pipeline")
     parser.add_argument('--data', type=str, required=True, help='Path to data.yaml')
@@ -20,56 +18,28 @@ def train_and_validate():
     parser.add_argument('--patience', type=int, default=30, help='Early stopping')
     parser.add_argument('--epochs', type=int, default=150)
     parser.add_argument('--batch', type=int, default=8)
-    parser.add_argument('--imgsz', type=int, default=1000)
+    parser.add_argument('--imgsz', type=int, default=1024)
     args = parser.parse_args()
 
-    os.environ["ULTRALYTICS_MLFLOW"] = "True"
-
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    project_root = os.path.abspath(os.path.join(current_dir, "../../../"))
+    project_root = os.path.abspath(os.path.join(current_dir, "../../../../"))
 
-    mlflow.set_tracking_uri(f"file://{os.path.join(project_root, 'mlruns')}")
-    mlflow.set_experiment("cerrado_tree_identifier")
-
-    if mlflow.active_run():
-        mlflow.end_run()
-
-    with mlflow.start_run(run_name=args.name):
-        model_path = os.path.join(project_root, "models", "yolo11n.pt")
-        model = YOLO(model_path)
-
-        if args.tags:
-            tags_list = args.tags.split(',')
-            for tag in tags_list:
-                key, value = tag.split(':')
-                mlflow.set_tag(key, value)
-        
-        mlflow.set_tag("model_version", "yolov11n")
-        mlflow.set_tag("project_phase", "no_augmentation")
+    model_path = os.path.join(project_root, "models", "yolo11n.pt")
+    model = YOLO(model_path)
     
-        results = model.train(
-            data=args.data,
-            epochs=args.epochs,
-            #patience=args.patience,
-            batch=args.batch,
-            imgsz=args.imgsz,
-            name=args.name,
-            exist_ok=True,
-            save=True,     
-            plots=True,
-            device=0,
-            project=os.path.join(project_root, "runs/yolo11n/no_augmentation")      
-        )
-
-        if results is not None:
-            metrics = results.results_dict
-            for k, v in metrics.items():
-                clean_name = k.replace("metrics/", "")
-                clean_name = clean_name.replace("(", "").replace(")", "")
-                clean_name = clean_name.replace(" ", "_")
-                mlflow.log_metric(clean_name, float(v))
-
-        mlflow.log_artifacts(f"runs/detect/{args.name}")
+    model.train(
+        data=args.data,
+        epochs=args.epochs,
+        patience=args.patience,
+        batch=args.batch,
+        imgsz=args.imgsz,
+        name=args.name,
+        exist_ok=True,
+        save=True,     
+        plots=True,
+        device=0,
+        project=os.path.join(project_root, "runs/yolo11n")      
+    )
 
     del model
     gc.collect()
