2860d8defae6234b35e5bbe324fce27db679f2a3
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-04-09 00:50:53 -0300

    :sparkles: feat: deactivating yolo online data augmentation

--- src/models/yolo11n/train/train.py
+++ src/models/yolo11n/train/train.py
@@ -14,6 +14,7 @@ def train_and_validate():
     parser = argparse.ArgumentParser(description="YOLO Training & Validation Pipeline")
     parser.add_argument('--data', type=str, required=True, help='Path to data.yaml')
     parser.add_argument('--patience', type=int, default=30, help='Early stopping')
+    parser.add_argument('--name', type=str, required=True, help='Unique run name')
     parser.add_argument('--epochs', type=int, default=150)
     parser.add_argument('--batch', type=int, default=8)
     parser.add_argument('--imgsz', type=int, default=1024)
@@ -31,11 +32,21 @@ def train_and_validate():
         patience=args.patience,
         batch=args.batch,
         imgsz=args.imgsz,
+        name=args.name,
         exist_ok=True,
         save=True,     
         plots=True,
         device=0,
-        project=os.path.join(project_root, "runs/yolo11n")      
+        project=os.path.join(project_root, "runs/yolo11n"),
+
+        # Deactivate online augmentation maked by Yolo
+        hsv_h=0.0, hsv_s=0.0, hsv_v=0.0, 
+        mosaic=0.0, mixup=0.0, copy_paste=0.0, 
+        scale=0.0, 
+        
+        degrees=15.0, 
+        translate=0.1,
+        fliplr=0.5      
     )
 
     del model
