947a45d959d18f27567d08feb639568794d3623a
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-04-09 18:44:45 -0300

    fixing directory path

--- src/models/yolo11n/eval/eval.py
+++ src/models/yolo11n/eval/eval.py
@@ -7,7 +7,7 @@ import torch
 def evaluate():
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    project_root = os.path.abspath(os.path.join(current_dir, "../../../"))
+    project_root = os.path.abspath(os.path.join(current_dir, "../../../../"))
     reports_dir = os.path.join(project_root, 'reports', 'evaluations')
 
     parser = argparse.ArgumentParser()
