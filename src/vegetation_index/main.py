:bug: fix: removing images normalization

src/vegetation_index/main.py
@@ -33,7 +33,7 @@ def calculate_indexes():
         for path in cur_imgs:
             if path.endswith('_D.JPG'):
                 rgb_path = path     
-            elif '_D_FUSED.JPG' in path:
+            elif '_D_FUSED.TIF' in path:
                 fused_path = path
             elif '_MS_NIR.TIF' in path:
                 nir_path = path
@@ -45,8 +45,8 @@ def calculate_indexes():
         rgb_obj = Path(rgb_path)
         stem_name = rgb_obj.stem
 
-        rgb_image = np.asarray(Image.open(str(rgb_path)))
-        fused_image = np.asarray(Image.open(str(fused_path)))
+        rgb_image = np.asarray(Image.open(str(rgb_path))).astype(np.float32) / 255.0
+        fused_image = tiff.imread(str(fused_path)).astype(np.float32)
 
         ndvi_image = calculate_ndvi(str(red_path), str(nir_path))
         ndre_image = calculate_ndre(str(re_path), str(nir_path))
@@ -62,33 +62,27 @@ def calculate_indexes():
         tiff.imwrite(ndre_output_dir / ndre_filename, ndre_image)
         
         # Stack and save indexes with RGB image
-        stack_rgb_ndvi = np.dstack((rgb_image, ndvi_image))
-        stack_rgb_ndre = np.dstack((rgb_image, ndre_image))
+        stack_rgb_ndvi = np.dstack((rgb_image, ndvi_image)).astype(np.float32)
+        stack_rgb_ndre = np.dstack((rgb_image, ndre_image)).astype(np.float32)
 
         new_stem_rgb_ndvi = stem_name.replace('_D', '_RGB_NDVI')
         new_stem_rgb_ndre = stem_name.replace('_D', '_RGB_NDRE')
 
-        rgb_ndvi_filename = f"{new_stem_rgb_ndvi}.TIF"
-        rgb_ndre_filename = f"{new_stem_rgb_ndre}.TIF"
-
-        tiff.imwrite(rgb_ndvi_output_dir / rgb_ndvi_filename, stack_rgb_ndvi)
-        tiff.imwrite(rgb_ndre_output_dir / rgb_ndre_filename, stack_rgb_ndre)
+        tiff.imwrite(rgb_ndvi_output_dir / f"{new_stem_rgb_ndvi}.TIF", stack_rgb_ndvi)
+        tiff.imwrite(rgb_ndre_output_dir / f"{new_stem_rgb_ndre}.TIF", stack_rgb_ndre)
 
         # Stack and save indexes with proposed Fusion
-        stack_fused_ndvi = np.dstack((fused_image, ndvi_image))
-        stack_fused_ndre = np.dstack((fused_image, ndre_image))
+        stack_fused_ndvi = np.dstack((fused_image, ndvi_image)).astype(np.float32)
+        stack_fused_ndre = np.dstack((fused_image, ndre_image)).astype(np.float32)
         
         new_stem_fused_ndvi = stem_name.replace('_D', '_FUSED_NDVI')
         new_stem_fused_ndre = stem_name.replace('_D', '_FUSED_NDRE')
 
-        fused_ndvi_filename = f"{new_stem_fused_ndvi}.TIF"
-        fused_ndre_filename = f"{new_stem_fused_ndre}.TIF"
-
-        tiff.imwrite(fused_ndvi_output_dir / fused_ndvi_filename, stack_fused_ndvi)
-        tiff.imwrite(fused_ndre_output_dir / fused_ndre_filename, stack_fused_ndre)
+        tiff.imwrite(fused_ndvi_output_dir / f"{new_stem_fused_ndvi}.TIF", stack_fused_ndvi)
+        tiff.imwrite(fused_ndre_output_dir / f"{new_stem_fused_ndre}.TIF", stack_fused_ndre)
 
 if __name__ == "__main__":
-    batch_path = Path("/datasets/projeto_cerrado/2025-06-16")
+    batch_path = Path("/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/2025-06-16")
 
     ndvi_output_dir = batch_path / "ndvi-imgs"
     ndre_output_dir = batch_path / "ndre-imgs"