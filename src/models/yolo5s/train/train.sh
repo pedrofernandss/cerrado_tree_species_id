72bae64ae6e69e3e3a5f6ffcc2fabf9afdf7a6d1
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-04-16 20:08:47 -0300

    fixing data path

--- src/models/yolo5s/train/train.sh
+++ src/models/yolo5s/train/train.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-LOG_DIR="/mnt/sdb-seagate/graduacao/logs/ana_pedro/yolo5s/no_augmentation"
+LOG_DIR="/mnt/sdb-seagate/graduacao/logs/ana_pedro/train/yolo5s"
 mkdir -p "$LOG_DIR"
 
 datasets=(
-    "fused" "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/datasets_no_augmentation/fused/data.yaml" "index:FUSED"
-    "fused-ndre" "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/datasets_no_augmentation/fused-ndre/data.yaml" "index:FUSED+NDRE"
-    "fused-ndvi"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/datasets_no_augmentation/fused-ndvi/data.yaml" "index:FUSED+NDVI"
-    "ndre"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/datasets_no_augmentation/ndre/data.yaml" "index:NDRE"
-    "ndvi"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/datasets_no_augmentation/ndvi/data.yaml" "index:NDVI"
-    "rgb"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/datasets_no_augmentation/rgb/data.yaml" "index:RGB"
-    "rgb-ndre"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/datasets_no_augmentation/rgb-ndre/data.yaml" "index:RGB+NDRE"
-    "rgb-ndvi" "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/datasets_no_augmentation/rgb-ndvi/data.yaml" "index:RGB+NDVI"
+    "fused" "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/fused/data.yaml" 
+    "fused-ndre" "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/fused-ndre/data.yaml" 
+    "fused-ndvi"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/fused-ndvi/data.yaml" 
+    "ndre"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/ndre/data.yaml" 
+    "ndvi"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/ndvi/data.yaml" 
+    "rgb"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/rgb/data.yaml" 
+    "rgb-ndre"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/rgb-ndre/data.yaml"
+    "rgb-ndvi" "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/rgb-ndvi/data.yaml" 
 )
 
-for ((i=0; i<${#datasets[@]}; i+=3)); do
+for ((i=0; i<${#datasets[@]}; i+=2)); do
     NAME="${datasets[$i]}"
     YAML="${datasets[$i+1]}"
-    TAGS="${datasets[$i+2]}"
 
     echo "----------------------------------------------------------"
     echo "Iniciando treinamendo do $NAME com o arquivo $YAML"
     echo "----------------------------------------------------------"
 
-    python3 train.py --data "$YAML" --name "$NAME" --tags "$TAGS" > "$LOG_DIR/$NAME.log" 2>&1
+    python3 train.py --data "$YAML" --name "$NAME" > "$LOG_DIR/$NAME.log" 2>&1
 
     echo "Finalizado: $NAME"
     sleep 10 
