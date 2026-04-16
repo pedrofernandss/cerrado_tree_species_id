98996f741710a2fd3538a5fd67a3fcbd23f7701d
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-04-16 20:09:10 -0300

    refactoring eval shell script for new data

--- src/models/yolo5n/eval/eval.sh
+++ src/models/yolo5n/eval/eval.sh
@@ -1,62 +1,40 @@
 #!/bin/bash
 
-LOG_DIR="/mnt/sdb-seagate/graduacao/logs/ana_pedro/evaluations/yolov5n"
+LOG_DIR="/mnt/sdb-seagate/graduacao/logs/ana_pedro/evaluations/yolo5n"
 mkdir -p "$LOG_DIR"
 
-DATASET_BASE_NO_AUG="/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/datasets_no_augmentation"
-DATASET_BASE_AUG="/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/datasets_augmented"
+DATASET="/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified"
 
 datasets=(
-    "fused" "index:FUSED"
-    "fused-ndre" "index:FUSED+NDRE"
-    "fused-ndvi" "index:FUSED+NDVI"
-    "ndre" "index:NDRE"
-    "ndvi" "index:NDVI"
-    "rgb" "index:RGB"
-    "rgb-ndre" "index:RGB+NDRE"
-    "rgb-ndvi" "index:RGB+NDVI"
+    "fused" 
+    "fused-ndre"
+    "fused-ndvi" 
+    "ndre" 
+    "ndvi" 
+    "rgb" 
+    "rgb-ndre" 
+    "rgb-ndvi" 
 )
 
-for ((i=0; i<${#datasets[@]}; i+=2)); do
+for ((i=0; i<${#datasets[@]}; i+=1)); do
     NAME="${datasets[$i]}"
-    TAG="${datasets[$i+1]}"
 
-    YAML_NO_AUG="$DATASET_BASE_NO_AUG/$NAME/data.yaml"
-    MODEL_NO_AUG="../../../runs/yolov5n/no_augmentation/$NAME/weights/best.pt"
-    RUN_NAME_NO_AUG="test_no_augmented_$NAME"
+    YAML="$DATASET/$NAME/data.yaml"
+    MODEL="../../../../runs/yolov5n/$NAME/weights/best.pt"
+    RUN_NAME="test_$NAME"
 
-    if [ -f "$MODEL_NO_AUG" ]; then
-        echo "Lançando Teste Sem Data Augmentation : $NAME"
-        find "$DATASET_BASE_NO_AUG/$NAME" -name "*.cache" -delete
+    if [ -f "$MODEL" ]; then
+        echo "Realizando teste : $NAME"
+        find "$DATASET/$NAME" -name "*.cache" -delete
         python3 eval.py \
-            --model "$MODEL_NO_AUG" \
-            --data "$YAML_NO_AUG" \
-            --name "$RUN_NAME_NO_AUG" \
-            --aug "no" \
-            --tags "$TAG" > "$LOG_DIR/$RUN_NAME_NO_AUG.log" 2>&1
+            --model "$MODEL" \
+            --data "$YAML" > "$LOG_DIR/$RUN_NAME.log" 2>&1
     else
-        echo "ERRO: Modelo não encontrado para $NAME em $MODEL_NO_AUG"
+        echo "ERRO: Modelo não encontrado para $NAME em $MODEL"
     fi
 
-    # YAML_AUG="$DATASET_BASE_AUG/$NAME/data.yaml"
-    # MODEL_AUG="../../../runs/yolov5n/augmented/$NAME/weights/best.pt"
-    # RUN_NAME_AUG="test_augmented_$NAME"
-
-    # if [ -f "$MODEL_AUG" ]; then
-    #     echo "Lançando Teste Augmented: $NAME"
-    #     find "$DATASET_BASE_AUG/$NAME" -name "*.cache" -delete
-    #     python3 eval.py \
-    #         --model "$MODEL_AUG" \
-    #         --data "$YAML_AUG" \
-    #         --name "$RUN_NAME_AUG" \
-    #         --aug "yes" \
-    #         --tags "$TAG" > "$LOG_DIR/$RUN_NAME_AUG.log" 2>&1
-    # else
-    #     echo "ERRO: Modelo Augmented não encontrado para $NAME em $MODEL_AUG"
-    # fi
-
-    # echo "Progresso: $NAME finalizado (Baseline & Augmented)."
-    # sleep 10 
+    echo "Progresso: $NAME finalizado."
+    sleep 10 
 done
 
-echo "Processo de avaliação finalizado! Confira os resultados no MLflow."
+echo "Processo de avaliação finalizado!"
