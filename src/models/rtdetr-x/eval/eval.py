63bee1cc2f950b8277bdf06b65c429f5c89d7b6c
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-04-20 20:56:04 -0300

    add eval pipeline for extra-large rtdetr

---
+++ src/models/rtdetr-x/eval/eval.py
@@ -0,0 +1,33 @@
+import os
+import gc
+import argparse
+from ultralytics import RTDETR
+import torch
+
+def evaluate():
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.abspath(os.path.join(current_dir, "../../../../"))
+    reports_dir = os.path.join(project_root, 'reports', 'evaluations', 'rtdetr-x')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, required=True, help='Path for best.pt')
+    parser.add_argument('--data', type=str, required=True, help='Path for data.yaml')
+    args = parser.parse_args()
+
+    model = RTDETR(args.model)
+
+    model.val(
+        data=args.data,
+        split='test',
+        project=reports_dir,
+        save=True,
+        plots=True
+    )
+    
+    del model
+    gc.collect()
+    torch.cuda.empty_cache()
+
+if __name__ == "__main__":
+    evaluate()
