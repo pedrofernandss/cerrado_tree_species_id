7159c9b277b1d422fdd69ff764fab473419b2aed
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-04-20 12:27:05 -0300

    add training pipeline for large rtdetr

---
+++ src/models/rtdetr-l/train/train.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+LOG_DIR="/mnt/sdb-seagate/graduacao/logs/ana_pedro/train/rt-detr-l"
+mkdir -p "$LOG_DIR"
+
+datasets=(
+    "fused" "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/fused/data.yaml" 
+    "fused-ndre" "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/fused-ndre/data.yaml" 
+    "fused-ndvi"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/fused-ndvi/data.yaml" 
+    "ndre"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/ndre/data.yaml" 
+    "ndvi"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/ndvi/data.yaml" 
+    "rgb"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/rgb/data.yaml" 
+    "rgb-ndre"  "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/rgb-ndre/data.yaml"
+    "rgb-ndvi" "/mnt/sdb-seagate/graduacao/datasets/projeto_cerrado/dataset_stratified/rgb-ndvi/data.yaml" 
+)
+
+for ((i=0; i<${#datasets[@]}; i+=2)); do
+    NAME="${datasets[$i]}"
+    YAML="${datasets[$i+1]}"
+
+    echo "----------------------------------------------------------"
+    echo "Iniciando treinamendo do $NAME com o arquivo $YAML"
+    echo "----------------------------------------------------------"
+
+    python3 train.py --data "$YAML" --name "$NAME" > "$LOG_DIR/$NAME.log" 2>&1
+
+    echo "Finalizado: $NAME"
+    sleep 10 
+done
+
+echo "Todos os 8 treinamentos foram concluídos!"
