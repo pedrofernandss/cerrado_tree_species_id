1c5bf4ecb591468228f8c6a85f8872c614d2a817
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-04-23 15:33:25 -0300

    script for yolo 5 small for multiple seed

--- src/models/yolo5s/train/train.py
+++ src/models/yolo5s/train/train.py
@@ -3,25 +3,24 @@ import os
 
 user_site = "/mnt/sdb-seagate/graduacao/python_userbase/ana_pedro/lib/python3.9/site-packages"
 if user_site not in sys.path:
-    sys.path.insert(0, user_site) 
+    sys.path.insert(0, user_site)
 
 import gc
 import torch
 import argparse
 from ultralytics import YOLO
 
-import mlflow
 
 def train_and_validate():
     parser = argparse.ArgumentParser(description="YOLO Training & Validation Pipeline")
     parser.add_argument('--data', type=str, required=True, help='Path to data.yaml')
     parser.add_argument('--name', type=str, required=True, help='Unique run name')
+    parser.add_argument('--seed', type=int, default=1, help='Random seed')
     args = parser.parse_args()
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
     project_root = os.path.abspath(os.path.join(current_dir, "../../../../"))
 
-    model_path = os.path.join(project_root, "models", "yolov5su.pt")
     model = YOLO('yolov5su.pt')
 
     model.train(
@@ -29,20 +28,21 @@ def train_and_validate():
         name=args.name,
         task='detect',
         exist_ok=True,
-        save=True,     
+        save=True,
         plots=True,
         device=1,
         epochs=100,
         batch=12,
         imgsz=640,
         pretrained=False,
-        seed=3,
-        project=os.path.join(project_root, "runs/yolov5s")      
+        seed=args.seed,
+        project=os.path.join(project_root, "runs", f"seed{args.seed}", "yolov5s")
     )
 
     del model
     gc.collect()
     torch.cuda.empty_cache()
+
 
 if __name__ == "__main__":
     train_and_validate()
