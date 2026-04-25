af91d63ddbc2ebf4101fce7fe4530c27fc52639f
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-04-24 22:11:35 -0300

    :recycle: refactor: refactoring evaluation python script

--- src/models/yolo11n/eval/eval.py
+++ src/models/yolo11n/eval/eval.py
@@ -4,16 +4,18 @@ import argparse
 from ultralytics import YOLO
 import torch
 
+
 def evaluate():
-
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    project_root = os.path.abspath(os.path.join(current_dir, "../../../../"))
-    reports_dir = os.path.join(project_root, 'reports', 'evaluations', 'yolo11n')
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, required=True, help='Path for best.pt')
     parser.add_argument('--data', type=str, required=True, help='Path for data.yaml')
+    parser.add_argument('--seed', type=int, required=True, help='Seed number')
+    parser.add_argument('--model_name', type=str, required=True, help='Model name (e.g. yolov5n)')
     args = parser.parse_args()
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.abspath(os.path.join(current_dir, "../../../../"))
+    reports_dir = os.path.join(project_root, 'reports', 'evaluations', f'seed{args.seed}', args.model_name)
 
     model = YOLO(args.model)
 
@@ -24,12 +26,14 @@ def evaluate():
         imgsz=640,
         project=reports_dir,
         save=True,
-        plots=True
+        plots=True,
+        device=1,
     )
-    
+
     del model
     gc.collect()
     torch.cuda.empty_cache()
+
 
 if __name__ == "__main__":
     evaluate()
--- src/models/yolov8s/eval/eval.py
+++ src/models/yolov8s/eval/eval.py
@@ -4,18 +4,18 @@ import argparse
 from ultralytics import YOLO
 import torch
 
-os.environ["ULTRALYTICS_MLFLOW"] = "True"
 
 def evaluate():
-
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    project_root = os.path.abspath(os.path.join(current_dir, "../../../../"))
-    reports_dir = os.path.join(project_root, 'reports', 'evaluations', 'yolo8s')
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, required=True, help='Path for best.pt')
     parser.add_argument('--data', type=str, required=True, help='Path for data.yaml')
+    parser.add_argument('--seed', type=int, required=True, help='Seed number')
+    parser.add_argument('--model_name', type=str, required=True, help='Model name (e.g. yolov5n)')
     args = parser.parse_args()
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.abspath(os.path.join(current_dir, "../../../../"))
+    reports_dir = os.path.join(project_root, 'reports', 'evaluations', f'seed{args.seed}', args.model_name)
 
     model = YOLO(args.model)
 
@@ -26,12 +26,14 @@ def evaluate():
         imgsz=640,
         project=reports_dir,
         save=True,
-        plots=True
+        plots=True,
+        device=1,
     )
-    
+
     del model
     gc.collect()
     torch.cuda.empty_cache()
+
 
 if __name__ == "__main__":
     evaluate()
