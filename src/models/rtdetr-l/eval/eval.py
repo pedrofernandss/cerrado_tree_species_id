9bce8465c518c7a9504c7daf77c02b38ab97681b
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-04-27 18:14:15 -0300

    :sparkles: feat: training ultranalytics rt-detr

--- src/models/rtdetr-l/eval/eval.py
+++ src/models/rtdetr-l/eval/eval.py
@@ -5,26 +5,30 @@ from ultralytics import RTDETR
 import torch
 
 def evaluate():
-
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    project_root = os.path.abspath(os.path.join(current_dir, "../../../../"))
-    reports_dir = os.path.join(project_root, 'reports', 'evaluations', 'rtdetr-l')
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, required=True, help='Path for best.pt')
     parser.add_argument('--data', type=str, required=True, help='Path for data.yaml')
+    parser.add_argument('--seed', type=int, required=True, help='Seed number')
+    parser.add_argument('--model_name', type=str, required=True, help='Model name (e.g. yolov5n)')
     args = parser.parse_args()
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.abspath(os.path.join(current_dir, "../../../../"))
+    reports_dir = os.path.join(project_root, 'reports', 'evaluations', f'seed{args.seed}', args.model_name)
 
     model = RTDETR(args.model)
 
     model.val(
         data=args.data,
         split='test',
+        task='detect',
+        imgsz=640,
         project=reports_dir,
         save=True,
-        plots=True
+        plots=True,
+        device=1,
     )
-    
+
     del model
     gc.collect()
     torch.cuda.empty_cache()
--- src/models/rtdetr-l/train/train.py
+++ src/models/rtdetr-l/train/train.py
@@ -1,41 +1,48 @@
+from ultralytics import RTDETR
+import argparse
+import torch
+import gc
 import sys
 import os
 
 user_site = "/mnt/sdb-seagate/graduacao/python_userbase/ana_pedro/lib/python3.9/site-packages"
 if user_site not in sys.path:
-    sys.path.insert(0, user_site) 
+    sys.path.insert(0, user_site)
 
-import gc
-import torch
-import argparse
-from ultralytics import RTDETR
 
 def train_and_validate():
-    parser = argparse.ArgumentParser(description="Large RT-DETR Training & Validation Pipeline")
-    parser.add_argument('--data', type=str, required=True, help='Path to data.yaml')
-    parser.add_argument('--name', type=str, required=True, help='Unique run name')
+    parser = argparse.ArgumentParser(description="rtdetr Training Pipeline")
+    parser.add_argument('--data', type=str, required=True,
+                        help='Path to data.yaml')
+    parser.add_argument('--name', type=str, required=True,
+                        help='Unique run name')
+    parser.add_argument('--seed', type=int, default=1, help='Random seed')
     args = parser.parse_args()
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
     project_root = os.path.abspath(os.path.join(current_dir, "../../../../"))
 
     model = RTDETR("rtdetr-l.pt")
-    
+
     model.train(
         data=args.data,
         name=args.name,
         exist_ok=True,
-        save=True,     
+        save=True,
         plots=True,
-        device=[0, 1],
-        batch=16,
+        device=1,
+        batch=8,
         epochs=50,
-        project=os.path.join(project_root, "runs/rt-detr-l"),  
+        seed=args.seed,
+        pretrained=False,
+        project=os.path.join(project_root, "runs",
+                             f"seed{args.seed}", "rtdetr-l")
     )
 
     del model
     gc.collect()
     torch.cuda.empty_cache()
+
 
 if __name__ == "__main__":
     train_and_validate()
